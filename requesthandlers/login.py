5f03098775a9995054ccccb09c9b4e82e626d22d
Ian Renton <ian@ianrenton.com>
2026-01-25 18:19:39 +0000

    Honour Tornado "next" URL properly

--- requesthandlers/login.py
+++ requesthandlers/login.py
@@ -5,16 +5,21 @@ class LoginHandler(BaseHandler):
     """Handler for login page, includes POSTing username and password as well as rendering the HTML"""
 
     def get(self):
-        # Redirect to the admin page if we are already logged in, as we can just skip this one
+        # Get the 'next' parameter from the query string if there was one. This is where we are going to forward to on
+        # successful login. Default to the admin dashboard.
+        next_url = self.get_argument("next", "/admin")
+
+        # Redirect to the 'next' URL if we are already logged in, as we can just skip the login
         if self.current_user:
-            self.redirect("/admin")
+            self.redirect(next_url)
             return
 
         # Get data we need to include in the template
         insecure_user_present = self.application.db.is_insecure_user_present()
 
-        # Render the template
-        self.render("login.html", insecure_user_present=insecure_user_present)
+        # Render the template. This includes a hidden field with the 'next' URL in it so we can get it back again in the
+        # POST method.
+        self.render("login.html", next=next_url, insecure_user_present=insecure_user_present)
 
     def post(self):
         """Handles POST requests for login page. If successful a session token will be created, stored in a cookie, and
@@ -23,6 +28,7 @@ class LoginHandler(BaseHandler):
         # Get request arguments
         username = self.get_argument("username")
         password = self.get_argument("password")
+        next_url = self.get_argument("next", "/admin")
 
         # Check that the username and password match a known user
         user_id = self.application.db.verify_user(username, password)
@@ -30,6 +36,6 @@ class LoginHandler(BaseHandler):
         if user_id:
             session_token = self.application.db.create_user_session(user_id)
             self.set_secure_cookie("session_token", session_token)
-            self.redirect("/admin")
+            self.redirect(next_url)
         else:
             self.write("Invalid credentials")
