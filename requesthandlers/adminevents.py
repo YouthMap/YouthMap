a41d60445a32bfc93fee0aca441e0c945e5d03c4
Ian Renton <ian@ianrenton.com>
2026-02-01 16:32:00 +0000

    Break events and stations into categories in the list interface

--- requesthandlers/adminevents.py
+++ requesthandlers/adminevents.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import tornado
 
 from requesthandlers.base import BaseHandler
@@ -9,7 +11,10 @@ class AdminEventsHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
         # Get data we need to include in the template
-        events = self.application.db.get_all_events()
+        events = sorted(self.application.db.get_all_events(), key=lambda x: x.start_time)
+        events_by_type = {"Past": [x for x in events if datetime.now() > x.end_time],
+                                 "Current": [x for x in events if x.start_time <= datetime.now() <= x.end_time],
+                                 "Future": [x for x in events if datetime.now() < x.start_time]}
 
         # Render the template
-        self.render("adminevents.html", events=events)
+        self.render("adminevents.html", events_by_type=events_by_type)
--- requesthandlers/adminstations.py
+++ requesthandlers/adminstations.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import tornado
 
 from requesthandlers.base import BaseHandler
@@ -9,8 +11,14 @@ class AdminStationsHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
         # Get data we need to include in the template
-        temp_stations = self.application.db.get_all_temporary_stations()
-        perm_stations = self.application.db.get_all_permanent_stations()
+        temp_stations = sorted(self.application.db.get_all_temporary_stations(), key=lambda x: x.start_time)
+        temp_stations_by_type = {"Past": [x for x in temp_stations if datetime.now() > x.end_time],
+                                 "Current": [x for x in temp_stations if x.start_time <= datetime.now() <= x.end_time],
+                                 "Future": [x for x in temp_stations if datetime.now() < x.start_time]}
+        perm_stations = sorted(self.application.db.get_all_permanent_stations(), key=lambda x: x.callsign)
+        perm_stations_by_type = {}
+        for station_type in self.application.db.get_all_permanent_station_types():
+            perm_stations_by_type[station_type.name] = [x for x in perm_stations if x.type.name == station_type.name]
 
         # Render the template
-        self.render("adminstations.html", temp_stations=temp_stations, perm_stations=perm_stations)
+        self.render("adminstations.html", temp_stations_by_type=temp_stations_by_type, perm_stations_by_type=perm_stations_by_type)
