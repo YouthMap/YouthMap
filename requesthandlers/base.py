1ee00270b8be6179ba200411ac7a0bfa82285d6b
Ian Renton <ian@ianrenton.com>
2026-03-09 17:53:19 +0000

    Set header meta content #65

--- requesthandlers/base.py
+++ requesthandlers/base.py
@@ -10,3 +10,9 @@ class BaseHandler(tornado.web.RequestHandler):
             return None
 
         return self.application.db.verify_user_session_token(session_token.decode('utf-8'))
+
+    def render(self, template_name, **kwargs):
+        config = self.application.db.get_config()
+        kwargs.setdefault('baseurl', config.base_url if config else '')
+        kwargs.setdefault('current_path', self.request.path)
+        super().render(template_name, **kwargs)
